b9cfa82d38ab1fbfbdc39816c0b1384e04cd9ac8
jhonatan <jhonatanlira@gmail.com>
2024-06-26 17:14:24 -0300

    Consumer Exemplo

---
+++ src/ConsumerExemplo.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Representa uma operação que aceita um argumento do tipo T e não retorna nenhum resultado.
+ * É utilizada principalmente para realizar ações, ou efeitos colaterais nos elementos do Stream sem modificar, ou
+ * retornar um valor.
+ */
+public class ConsumerExemplo {
+    public static void main(String[] args) {
+        //Criar uma lista de numeros inteiros
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+
+        //Usar o Consumer com expressão Lambda para imprimir numeros pares
+        Consumer<Integer>imprimirNumerosPar = numero ->{
+            if(numero%2==0){
+                System.out.println(numero);
+            }
+        };
+        //Usar o Consumer para imprimir numeros pares no Stream
+        //numeros.stream().forEach(imprimirNumerosPar);
+       /*numeros.stream().forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer n) {
+                if (n % 2==0){
+                    System.out.println(n);
+                }
+            }
+        });*/
+        numeros.stream().forEach( n -> {
+                if (n % 2==0){
+                    System.out.println(n);
+                }
+            }
+        );
+    }
+}
